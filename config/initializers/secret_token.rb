f416d3c60cf8ac2d9c6d0dc61b0fed7a9c066d89
Irvin Zhan <izhan@princeton.edu>
2014-03-26 19:20:03 -0400

    initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrackIt::Application.config.secret_key_base = 'b35c10545dae46ab920fc1a6e9b7c4da6d031c254c35d5d9ea2e0e81388049fb106471cefd9afb685e846f4a1e7c8cdb288dae66fef9096ef67e973bb39d223e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrackIt::Application.config.session_store :cookie_store, key: '_TrackIt_session'
