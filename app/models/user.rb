83214a56ddea9b5625bc8587601f7f7ca041ccc1
Irvin Zhan <izhan@princeton.edu>
2014-04-03 17:51:58 -0400

    adding confirmable to devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence:   true,
-                    format:     { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
   validates :name, presence: true
 end
