35c2da11855337dfb31e5b5e0c254073f8bed1aa
Irvin Zhan <izhan@princeton.edu>
2014-04-03 11:35:08 -0400

    adding url parsing for best buy

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,15 +11,32 @@ class StaticPagesController < ApplicationController
   end
 
   def dashboard
-    if params[:sku_number]
+    if params[:sku_number] && url = params[:sku_number].match(/(?<sku_number>[[:digit:]]{7}).p/)
+      begin
+        puts url[:sku_number]
+        @bestbuy = JSON.parse(open("http://api.remix.bestbuy.com/v1/products(sku=#{url[:sku_number]})?apiKey=xwfq3c3bekh3u2mnz3yu532f&format=json").read)
+        if @bestbuy["total"] == 0
+          @bestbuy = nil
+          flash[:danger] = "SKU Number Invalid.  Please try again."
+        end
+      rescue
+        flash[:danger] = "BestBuy URL Invalid.  Please try again."
+      end
+    elsif params[:sku_number] && /[[:digit:]]{7}/.match(params[:sku_number])
       begin
         @bestbuy = JSON.parse(open("http://api.remix.bestbuy.com/v1/products(sku=#{params[:sku_number]})?apiKey=xwfq3c3bekh3u2mnz3yu532f&format=json").read)
+        if @bestbuy["total"] == 0
+          @bestbuy = nil
+          flash[:danger] = "SKU Number Invalid.  Please try again."
+        end
       rescue
-        @bestbuy = {}
-        @bestbuy["products"] = [{}]
-        @bestbuy["products"][0]["name"] = "SKU Number Invalid"
-        @bestbuy["products"][0]["regularPrice"] = "Please Try Again"
+        puts "HUGE ERROR.  SKU_NUMBER:" # should never reach here
+        puts params[:sku_number]
+        @bestbuy = nil
+        flash[:danger] = "SKU Number Invalid.  Please try again."
       end
+    else
+      flash[:danger] = "Please try again."
     end
   end
 end
