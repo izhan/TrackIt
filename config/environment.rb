64a975f571bf808de457902b239036de0df4e9b1
Irvin Zhan <izhan@princeton.edu>
2014-04-03 20:14:33 -0400

    adding sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 TrackIt::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.delivery_method = :smtp
+config.action_mailer.default_url_options = { :host => 'http://fathomless-stream-5982.herokuapp.com/' }
