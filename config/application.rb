768e91e0b98535d77bbb3eefcce87052bb1fda3b
Irvin Zhan <irvinzhan@nat-oitwireless-inside-vapornet100-a-3717.princeton.edu>
2014-04-21 22:44:03 -0400

    xframe allows all

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module TrackIt
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # TODO move this to specific controllers
+    config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
   end
 end
