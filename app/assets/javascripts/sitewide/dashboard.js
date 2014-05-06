ca2bda676d629364d11082f31289660dad85c94c
Haley Beck <hebeck@princeton.edu>
2014-05-06 12:58:28 -0400

    added list.js to dashboard

--- app/assets/javascripts/sitewide/dashboard.js
+++ app/assets/javascripts/sitewide/dashboard.js
@@ -1,17 +1,7 @@
-$(document).ready(function(){
-	$("#search-trackers").click(function(){
-		var array = $('.media');
-		var arrayNames = $('.media-wrapper-heading');
-		var str = '';
-		var searchText = $('#search-tracker').val().toLowerCase();
-		for (var i = 0; i < arrayNames.length; i++)
-		{
-			str = '' + $(arrayNames[i]).text().toLowerCase();
-			if (str.indexOf(searchText) != -1) {
-				$(array[i]).show(); }
-			else
-				$(array[i]).hide();
+$( document ).ready(function() {
+	var options = {
+	  valueNames: [ 'product-name', 'price-orig', 'price-current' ]
+	};
 
-		};
-	});	
+	var trackerList = new List('tracker-results', options);
 });
