9d854c1bc8ddfd500cf2993cbd59bd9f3bfd0205
Irvin Zhan <izhan@princeton.edu>
2014-05-11 01:07:22 -0400

    focus true by default for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,9 @@ Spork.prefork do
   ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
   RSpec.configure do |config|
+    config.treat_symbols_as_metadata_keys_with_true_values = true
+    config.run_all_when_everything_filtered = true
+    config.filter_run :focus
     # ## Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
